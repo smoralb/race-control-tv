b72fbd8c8a22c580ec8fa67cdf314563f29dd881
Leonardo Rossetto <leonardoxh@gmail.com>
2021-07-23 09:28:15 +0200

    Fixing presentation list row

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.leonardoxh.f1"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 28
-        versionName = "2.2.4"
+        versionCode = 29
+        versionName = "2.2.5"
 
         buildConfigField("String", "DEFAULT_USER_AGENT", "\"RaceControl f1viewer\"")
     }
